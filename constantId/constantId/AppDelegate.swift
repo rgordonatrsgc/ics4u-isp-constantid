8f823672da3055439e00725f26624931f81a454b
Jeffrey Goldsmith <jgoldsmith@rsgc.on.ca>
2017-02-13 09:18:30 -0500

    Added xcode project

---
+++ constantId/constantId/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  constantId
+//
+//  Created by Jeffrey on 2017-02-13.
+//  Copyright © 2017 Jeffrey. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ constantId/constantId/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  constantId
+//
+//  Created by Jeffrey on 2017-02-13.
+//  Copyright © 2017 Jeffrey. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
